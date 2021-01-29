ddfbf09a056e88ff148a3d4c89060aa93d350be8
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-01-30 04:24:22 +0530

    created a different page for lati and longi ss

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
-// const { ObjectId } = require('mongodb')
 const express = require('express')
 const bp = require('body-parser')
-const http = require('http')
 const app = express()
-const mongoose = require('mongoose')
-const positions = require('./data')
 const home_page = require('./pages/home')
 const detail_page = require('./pages/details')
+const lati_and_longi_page = require('./pages/lati_and_longi')
 const next_page = require('./pages/next')
-// const liverreload = require('livereload')
-let obj = {}
-let obj1
-
-
-
 
 app.use(bp.urlencoded({extended:true}))
 
 app.use(home_page)
 
-// app.
-
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-// console.log(__dirname);
+app.use(detail_page)
 
 app.set('views' , './views')
 app.set('view engine', 'ejs')
 
-app.use(detail_page)
-
+app.use(lati_and_longi_page)
 app.use(next_page)
 
-// app.post('/nextpage',function(req,res){
-//     res.sendFile(process.cwd()+'/'+'nextpage.html')
-// })
 
 
 app.listen(5555,function(){
